c007af12244bb329f127ab94dbfb6908604e66a6
RoshanKhalinG <102815285+RoshanKhalinG@users.noreply.github.com>
2023-12-28 21:46:03 +0545

    Create Object.java

---
+++ Object.java
@@ -0,0 +1,13 @@
+public class Object {
+    String name;
+    public void show(){
+        System.out.println(name);
+    }
+}
+class demo{
+    public static void main(String[] args) {
+        Object obj = new Object();
+        obj.name = "Hello Universe";
+        obj.show();
+    }
+}
