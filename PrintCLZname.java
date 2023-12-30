df065130c6bf97fee1141311b5974cbce3437937
RoshanKhalinG <102815285+RoshanKhalinG@users.noreply.github.com>
2023-12-30 21:37:08 +0545

    Create PrintCLZname.java

---
+++ PrintCLZname.java
@@ -0,0 +1,9 @@
+public class PrintCLZname {
+   
+    public static void main(String args[]) {
+         int i;
+        for(i=1; i<=10; i++){
+            System.out.println("Citizen Collge");
+        }
+    }
+}
