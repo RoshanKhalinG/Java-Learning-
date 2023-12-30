0d2214c2ec0dad1c2b1a3f5eb8f1c2d663b4ec98
RoshanKhalinG <102815285+RoshanKhalinG@users.noreply.github.com>
2023-12-30 21:42:20 +0545

    Create IfElse.java

---
+++ IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int a=10;
+
+        if(a<20){
+            System.out.println("This is IF statement in Java.");
+        }
+        else{
+            System.out.println("This is Else statement.");
+        }
+    }
+}
