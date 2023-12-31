396d4794e04b5839ee9490425d18862ccd7d1c0d
RoshanKhalinG <102815285+RoshanKhalinG@users.noreply.github.com>
2023-12-31 21:12:25 +0545

    Create WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int x=5;
+
+        while (x<10) {
+            System.out.println("Value of X :"+x);
+            x++;
+        }
+    }
+}
