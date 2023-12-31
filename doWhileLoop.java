0ac0566fa6397edf20a851c413d819eae2703ce2
RoshanKhalinG <102815285+RoshanKhalinG@users.noreply.github.com>
2023-12-31 21:08:56 +0545

    Create doWhileLoop.java

---
+++ doWhileLoop.java
@@ -0,0 +1,10 @@
+public class doWhileLoop {
+    public static void main(String[] args) {
+        int x=10;
+
+        do{
+            System.out.println("The Value of x :"+x);
+            x++;
+        }while(x<12);
+    }
+}
